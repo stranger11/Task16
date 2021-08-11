dc90a4d31e85dd384bce41bc71ca4caf682112d0
Yan <yanislav888@mail.ru>
2021-08-11 17:05:04 +0300

    16-1

--- app/src/main/java/com/example/task16/MainActivity.kt
+++ app/src/main/java/com/example/task16/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.task16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task16.data.ModelItem
+import com.example.task16.util.GSON_PHONE_DIRECTORY_
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jsonList = GSON_PHONE_DIRECTORY_
+        val gson = Gson()
+        val listOfModels = object : TypeToken<List<ModelItem>>() {}.type
+        val models: List<ModelItem> = gson.fromJson(jsonList, listOfModels)
+
+        val listOfModelsForReading = models.forEachIndexed  { idx, tut -> println("> Item ${idx}:\n${tut}") }
+        Timber.d(listOfModelsForReading.toString())
     }
 }
---
+++ app/src/main/java/com/example/task16/data/Model.kt
@@ -0,0 +1,6 @@
+package com.example.task16.data
+
+
+import com.google.gson.annotations.SerializedName
+
+class Model : ArrayList<ModelItem>()
